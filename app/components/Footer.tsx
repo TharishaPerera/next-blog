c050e4dc54219724956520e3a12376210fe8addd
TharishaPerera <tharishaperera@gmail.com>
2023-09-19 10:19:08 +0530

    added footer links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,6 +4,14 @@ import { FaUserAlt } from 'react-icons/fa';
 import { AiOutlineLinkedin } from 'react-icons/ai';
 import { BsInstagram, BsLinkedin, BsGithub, BsFacebook } from 'react-icons/bs';
 
+const socialLinks = {
+  linkedIn: 'https://www.linkedin.com/in/tharisha-perera/',
+  github: 'https://github.com/TharishaPerera',
+  portfolio: 'https://www.linkedin.com/in/tharisha-perera/',
+  facebook: 'https://www.facebook.com/profile.php?id=100007492698410',
+  instagram: 'https://www.instagram.com/tharisha_perera/',
+}
+
 const Footer = () => {
   const year = new Date();
   return (
@@ -13,19 +21,19 @@ const Footer = () => {
       </aside>
       <ul className="flex ">
         <li>
-          <Link className="" href="#"><AiOutlineLinkedin className="w-6 h-6 rounded-lg hover:-translate-y-1 duration-300" /></Link>
+          <Link className="" href={socialLinks.linkedIn} legacyBehavior><a target="_blank"><AiOutlineLinkedin className="w-6 h-6 rounded-lg hover:-translate-y-1 duration-300" /></a></Link>
         </li>
         <li>
-          <Link className="" href="#"><BsGithub className="w-5 h-5 rounded-md hover:-translate-y-1 mx-2 duration-300" /></Link>
+          <Link className="" href={socialLinks.github} legacyBehavior><a target="_blank"><BsGithub className="w-5 h-5 rounded-md hover:-translate-y-1 mx-2 duration-300" /></a></Link>
         </li>
         <li>
-          <Link className="" href="#"><FaUserAlt className="w-5 h-5 rounded-md hover:-translate-y-1 mx-2 duration-300" /></Link>
+          <Link className="" href={socialLinks.portfolio} legacyBehavior><a target="_blank"><FaUserAlt className="w-5 h-5 rounded-md hover:-translate-y-1 mx-2 duration-300" /></a></Link>
         </li>
         <li>
-          <Link className="" href="#"><BsFacebook className="w-5 h-5 rounded-md hover:-translate-y-1 mx-2 duration-300" /></Link>
+          <Link className="" href={socialLinks.facebook} legacyBehavior><a target="_blank"><BsFacebook className="w-5 h-5 rounded-md hover:-translate-y-1 mx-2 duration-300" /></a></Link>
         </li>
         <li>
-          <Link className="" href="#"><BsInstagram className="w-5 h-5 rounded-md hover:-translate-y-1 duration-300" /></Link>
+          <Link className="" href={socialLinks.instagram} legacyBehavior><a target="_blank"><BsInstagram className="w-5 h-5 rounded-md hover:-translate-y-1 duration-300" /></a></Link>
         </li>
       </ul>
     </footer>
